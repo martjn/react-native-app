0679f514e4a8e24a93c2ddbe7019fa7b05f34a62
Student <student@WS-24239.edu.voco.ee>
2023-10-11 15:09:30 +0300

    Create favorites screen

--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./styles";
+import { products } from "../../../data/products";
+import FavoriteItem from "../../../components/FavoriteItem";
+import Header from "../../../components/Header";
 
 const Favorites = () => {
+  const renderItem = ({ item }) => {
+    return <FavoriteItem {...item} />;
+  };
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Text>Favorites</Text>
+        <Header title="Favorites" />
+        <FlatList
+          data={products}
+          renderItem={renderItem}
+          keyExtractor={(item) => String(item.id)}
+        />
       </View>
     </SafeAreaView>
   );
 };
 
-export default Favorites;
+export default React.memo(Favorites);
--- src/screens/app/Favorites/styles.js
+++ src/screens/app/Favorites/styles.js
@@ -2,14 +2,6 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    textAlign: "center",
-    justifyContent: "center",
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    alignItems: "center",
-    height: "100%",
     padding: 24,
-    borderWidth: 1,
   },
 });
